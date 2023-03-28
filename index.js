e7a008290a7946f621184350f338d5fca4ada2e2
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-28 20:58:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ worker1 = ["Gray", "Worm", "Security", 1];
 
 let twoRows = [
   ["moe", "sizlak", "barkeep", 2],
-  ["bartholomew", "simpson", "scamp", 3],
+  //["bartholomew", "simpson", "scamp", 3],
 ];
 
 let dataEmployees = [
@@ -36,7 +36,67 @@ function createEmployeeRecords(array) {
 }
 
 function createTimeInEvent(obj, dataStamp) {
-    
+  const date = dataStamp.split(" ")[0];
+  const time = parseInt(dataStamp.split(" ")[1]);
+  let timeInEntries = {
+    type: "TimeIn",
+    hour: time,
+    date: date,
+  };
+  obj.timeInEvents.push(timeInEntries);
+  return obj;
 }
 
-//console.log(createEmployeeRecords(dataEmployees));
+function createTimeOutEvent(obj, dataStamp) {
+  const date = dataStamp.split(" ")[0];
+  const time = parseInt(dataStamp.split(" ")[1]);
+  let timeOutEntries = {
+    type: "TimeOut",
+    hour: time,
+    date: date,
+  };
+  obj.timeOutEvents.push(timeOutEntries);
+  return obj;
+}
+
+function hoursWorkedOnDate(obj, date) {
+  const timeIn = obj.timeInEvents.find((data) => data.date === date);
+  const timeOut = obj.timeOutEvents.find((data) => data.date === date);
+
+  if (timeIn && timeOut) {
+    const timeInHours = Math.floor(timeIn.hour / 100);
+    const timeOutHours = Math.floor(timeOut.hour / 100);
+    const timeInMinutes = timeIn.hour % 100;
+    const timeOutMinutes = timeOut.hour % 100;
+
+    const hoursWorked =
+      timeOutHours + timeOutMinutes / 60 - (timeInHours + timeInMinutes / 60);
+
+    return hoursWorked;
+  }
+  return error;
+}
+
+function wagesEarnedOnDate(obj, date) {
+  const hours = hoursWorkedOnDate(obj, date);
+  const payPerHour = obj.payPerHour;
+  const pay = hours * payPerHour;
+  return pay;
+}
+
+function allWagesFor(employee) {
+  const dates = employee.timeInEvents.map((event) => event.date);
+  const totalPay = dates.reduce(
+    (total, date) => total + wagesEarnedOnDate(employee, date),
+    0
+  );
+  return totalPay;
+}
+function calculatePayroll(employees) {
+  const payRoll = employees.reduce(
+    (totalPay, employee) => totalPay + allWagesFor(employee),
+    0
+  );
+
+  return payRoll;
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const myObj = {
+  firstName: "Gray",
+  familyName: "Worm",
+  title: "Security",
+  payPerHour: 1,
+  timeInEvents: [{ type: "TimeIn", hour: 0800, date: "2014-02-28" }],
+  timeOutEvents: [{ type: "TimeOut", hour: 1400, date: "2014-02-28" }],
+};
